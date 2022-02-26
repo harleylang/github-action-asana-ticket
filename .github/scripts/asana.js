0a130970cc1fab09bd568c5b64b87f9f9fa79f18
Harley Lang (Desktop) <aba@harleylang.com>
2022-02-26 20:18:45 +0000

    asana.yml: fix: github action take 8

--- .github/scripts/asana.js
+++ .github/scripts/asana.js
@@ -10,7 +10,7 @@ const client = asana.Client.create().useAccessToken(ASANA_SECRET);
 
 console.log(GITHUB_CONTEXT, JSON.parse(GITHUB_CONTEXT));
 
-console.log(GITHUB_CONTENT.event)
+console.log(GITHUB_CONTEXT.event)
 
 const name = GITHUB_CONTEXT.event.issue.title;
 const url = GITHUB_CONTEXT.event.issue.html_url;
