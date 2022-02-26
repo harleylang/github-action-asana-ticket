a0d4237f7f350eae6d1534062a95226ed0548280
Harley Lang (Desktop) <aba@harleylang.com>
2022-02-26 14:47:47 +0000

    *: add: writed up asana action

--- .github/scripts/asana.js
+++ .github/scripts/asana.js
@@ -1,1 +1,24 @@
-console.log('Hello world');
+const asana = require('asana');
+require('dotenv').config();
+
+const ASANA_SECRET = process.env.ASANA_SECRET;
+const ASANA_PROJECT = process.env.ASANA_PROJECT;
+const ASANA_SECTION = process.env.ASANA_SECTION;
+const GITHUB_CONTEXT = process.env.GITHUB_CONTEXT;
+
+const client = asana.Client.create().useAccessToken(ASANA_SECRET);
+
+console.log(GITHUB_CONTEXT);
+
+// docs for creating tasks:
+// https://developers.asana.com/docs/create-a-task
+client.tasks.createTask({
+    name: 'TEST',
+    projects: [ASANA_PROJECT],
+}).then((result) => {
+    // docs for moving to specific sections:
+    // https://developers.asana.com/docs/add-task-to-section
+    client.sections.addTaskForSection(ASANA_SECTION, {
+        task: result.gid
+    })
+});
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # github-action-asana-ticket
 Scripts to trigger asana tickets when issues are opened in a github repo
+
+## Setup
+
+1) Setup an asana personal-access-token.
+2) Place the personal-access-token into github secrets as `ASANA_SECRET`
+3) Use the [asana api explorer](https://developers.asana.com/explorer) to identify the target project and section gid.
+    a) In github secrets, set `ASANA_PROJECT` to the project gid.
+    b) In github secrets, set `ASANA_SECTION` to the section gid.
